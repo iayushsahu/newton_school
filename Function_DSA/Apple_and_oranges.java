14c24ad415c6311d78f47a55fbaa5fab92f5f746
Ayush <87942664+iayushsahu@users.noreply.github.com>
2022-05-06 20:54:30 +0530

    Add files via upload

---
+++ Function_DSA/Apple_and_oranges.java
@@ -0,0 +1,4 @@
+static int LikesBoth(int N, int A, int B){
+    //Enter your code here
+    return(A+B-N);
+  }
---
+++ Function_DSA/Mohit_and_average.java
@@ -0,0 +1,4 @@
+static int Average(int A,int B, int C){
+    //Enter your code here
+        return ((A+B+C)/3);
+    }
